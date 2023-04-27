2bf628ec6e32f03869fd5c8ada7db6814bdaaa87
diego rodriguez <diego.rodriguez.ramirez@mi.unc.edu.ar>
2023-04-27 12:10:29 -0300

    [ADD] PATH

---
+++ main.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+#define BATTERY_PATH "/sys/class/power_supply/BAT0/capacity"
+#define BATTERY_STATUS_PATH "/sys/class/power_supply/BAT0/status"
+
+int 
+main()
+{
+       
+	return 0;
+}
