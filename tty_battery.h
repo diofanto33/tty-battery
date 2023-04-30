56523dfb63a19cd577a00489c26cb845f36601a4
diego rodriguez <diego.rodriguez.ramirez@mi.unc.edu.ar>
2023-04-30 15:03:31 -0300

    [ADD] struct

---
+++ tty_battery.h
@@ -0,0 +1,34 @@
+#ifndef TTYS_BATTERY_H
+#define TTYS_BATTERY_H
+
+#include <assert.h>
+#include <stdbool.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+struct
+{
+	int x, y, w, h;      
+    int a, b;
+} geometry;
+
+/* prototypes */
+void draw_number(int n, int x, int y);
+void draw_battery(int x, int y, int width, int height, int percent);
+
+/* Number matrix */
+const bool number[][15] =
+{
+     {1,1,1,1,0,1,1,0,1,1,0,1,1,1,1}, /* 0 */
+     {0,0,1,0,0,1,0,0,1,0,0,1,0,0,1}, /* 1 */
+     {1,1,1,0,0,1,1,1,1,1,0,0,1,1,1}, /* 2 */
+     {1,1,1,0,0,1,1,1,1,0,0,1,1,1,1}, /* 3 */
+     {1,0,1,1,0,1,1,1,1,0,0,1,0,0,1}, /* 4 */
+     {1,1,1,1,0,0,1,1,1,0,0,1,1,1,1}, /* 5 */
+     {1,1,1,1,0,0,1,1,1,1,0,1,1,1,1}, /* 6 */
+     {1,1,1,0,0,1,0,0,1,0,0,1,0,0,1}, /* 7 */
+     {1,1,1,1,0,1,1,1,1,1,0,1,1,1,1}, /* 8 */
+     {1,1,1,1,0,1,1,1,1,0,0,1,1,1,1}, /* 9 */
+};
+
+#endif // TTYS_BATTERY_H
