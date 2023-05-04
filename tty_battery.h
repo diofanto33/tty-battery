91cc99e5275caa4f8ff7aec2a076cfe3875d8182
diego rodriguez <diego.rodriguez.ramirez@mi.unc.edu.ar>
2023-05-04 10:17:30 -0300

    [UPD] tty-battery_t

--- tty_battery.h
+++ tty_battery.h
@@ -5,19 +5,56 @@
 #include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
-struct ttybattery 
-{
+
+struct ttybattery {
     /* while() boolean */
     bool running;
-	
-	/* geometry */
-	struct
-	{
-		int x, y, w, h;      
-    	int a, b;
-	} geometry;
 
-} ttybattery_t;
+    /* Running option */
+    struct {
+        bool screensaver;
+        bool center;
+        char format[100];
+        int color;
+        bool bold;
+        long delay;
+        bool blink;
+        long nsdelay;
+    } option;
+
+    /* Battery state */
+    struct {
+        int charge;
+        int capacity;
+        int time_remaining;
+        int temperature;
+        char status[20];
+    } state;
+
+    /* Geometry */
+    struct {
+        int x, y, w, h;
+        int a, b;
+    } geometry;
+
+    /* Multiple battery support */
+    int num_batteries;
+    struct {
+        int charge;
+        int capacity;
+        int time_remaining;
+        int temperature;
+        char status[20];
+        int x, y, w, h;
+    } *batteries;
+
+    /* Low battery notification */
+    bool notify_low_battery;
+    int low_battery_threshold;
+
+    /* Power source notification */
+    bool notify_power_source;
+};
 
 /* prototypes */
 void draw_number(int n, int x, int y);
