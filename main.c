4ff5e145fc8c20b820aaa2257002b0a83807f18e
diego rodriguez <diego.rodriguez.ramirez@mi.unc.edu.ar>
2023-05-04 10:52:52 -0300

    [UPD] v0

--- main.c
+++ main.c
@@ -2,16 +2,50 @@
 #include <stdlib.h>
 #include <unistd.h>
 
-int main() {
-    while (1) {
-		fflush(stdout);
-        FILE *fp = fopen("/sys/class/power_supply/BAT0/capacity", "r");
-        int battery_percentage;
-        fscanf(fp, "%d", &battery_percentage);
-        fclose(fp);
+#define BATTERY_PATH "/sys/class/power_supply/BAT0/capacity"
+#define BATTERY_STATUS_PATH "/sys/class/power_supply/BAT0/status"
 
-        printf("Battery percentage: %d%%", battery_percentage);
-        fflush(stdout);
+int main() {
+	
+    int res;
+	FILE *fp = NULL;
+	char battery_status[20];
+	unsigned int battery_percentage;
+	
+	while (1) {
+        
+		fp = fopen(BATTERY_PATH, "r");
+
+		if(fp == NULL)
+		{
+			perror("Error while opening battery file");
+			exit(EXIT_FAILURE);
+		}
+        
+        res = fscanf(fp, "%d", &battery_percentage);
+		if(res != 1)
+		{
+			perror("Error while reading battery percentage");
+			exit(EXIT_FAILURE);
+		}
+		fclose(fp);
+
+		fp = fopen(BATTERY_STATUS_PATH, "r");
+		if(fp == NULL)
+		{
+			perror("Error while opening battery status file");
+			exit(EXIT_FAILURE);
+		}
+	
+		res = fscanf(fp, "%s", battery_status);
+		if(res != 1)
+		{
+			perror("Error while reading battery status");
+			exit(EXIT_FAILURE);
+		}
+
+        printf("Battery percentage: %d%%\n", battery_percentage);
+        printf("\nBattery status: %s\n", battery_status);
 
         sleep(1);
     }
---
+++ tty_battery.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "tty_battery.h"
+
+
+void
+draw_number(int n, int x, int y)
+{
+    
+     return;
+}
